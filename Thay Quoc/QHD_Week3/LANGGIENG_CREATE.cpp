f701f694ed5c7d38a87e75ae89ddddd6d9cd7b83
Cuong Tran <tvc208@gmail.com>
2016-06-24 13:01:29 +0700

    Fix tabs

--- Thay Quoc/QHD_Week3/LANGGIENG_CREATE.cpp
+++ Thay Quoc/QHD_Week3/LANGGIENG_CREATE.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	ofstream out("LANGGIENG.INP");
-	out << "1000" << endl;
-	for(int i = 0; i < 1000000; i++) out << "1 ";
+    ofstream out("LANGGIENG.INP");
+    out << "1000" << endl;
+    for(int i = 0; i < 1000000; i++) out << "1 ";
 }
