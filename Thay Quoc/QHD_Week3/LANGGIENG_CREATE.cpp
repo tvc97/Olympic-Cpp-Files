81ce483487b71149407924b9e33ec9bf4ce4d5c2
Cuong Tran <tvc208@gmail.com>
2016-06-24 12:59:07 +0700

    Update 24/6

---
+++ Thay Quoc/QHD_Week3/LANGGIENG_CREATE.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	ofstream out("LANGGIENG.INP");
+	out << "1000" << endl;
+	for(int i = 0; i < 1000000; i++) out << "1 ";
+}
